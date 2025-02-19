9c1e4d8aefe18e16eee2ae6254c939df30ff345d
janakniraula <janakniraula@users.noreply.github.com>
2025-02-19 20:36:59 +0545

    Add ButtonSheet

--- app/src/main/java/com/android/navigationdrawer/HomeScreen.kt
+++ app/src/main/java/com/android/navigationdrawer/HomeScreen.kt
@@ -2,15 +2,19 @@ package com.android.navigationdrawer
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(onOpenBottomSheet: () -> Unit) {
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -20,5 +24,13 @@ fun HomeScreen() {
             text = "Home Screen",
             style = MaterialTheme.typography.headlineLarge
         )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Button(onClick = {
+            onOpenBottomSheet()
+        }) {
+            Text(text = "Open Bottom Sheet")
+        }
     }
 }
--- app/src/main/java/com/android/navigationdrawer/NavigationDrawerUI.kt
+++ app/src/main/java/com/android/navigationdrawer/NavigationDrawerUI.kt
@@ -2,8 +2,11 @@ package com.android.navigationdrawer
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
@@ -11,10 +14,12 @@ import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Button
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
@@ -22,10 +27,13 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberDrawerState
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -50,6 +58,10 @@ fun NavigationDrawerUI() {
     val scope = rememberCoroutineScope()
 
     val selectedItem = remember { mutableStateOf("Home") }
+
+    var showBottomSheet by remember { mutableStateOf(false) }
+
+    val sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = false)
 
     val screens = listOf(
         Screens(title = "Home", Icons.Default.Home),
@@ -89,7 +101,12 @@ fun NavigationDrawerUI() {
                                 }
                             },
                             modifier = Modifier.padding(vertical = 8.dp),
-                            icon = { Icon(imageVector = item.icons, contentDescription = item.title) }
+                            icon = {
+                                Icon(
+                                    imageVector = item.icons,
+                                    contentDescription = item.title
+                                )
+                            }
                         )
                     }
                 }
@@ -120,11 +137,45 @@ fun NavigationDrawerUI() {
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 when (selectedItem.value) {
-                    "Home" -> HomeScreen()
+                    "Home" -> HomeScreen(onOpenBottomSheet = {showBottomSheet = true})
                     "Profile" -> ProfileScreen()
                     "Settings" -> SettingsScreen()
                 }
+                if (showBottomSheet) {
+                    ModalBottomSheet(
+                        onDismissRequest = {
+                            showBottomSheet = false
+                        },
+                        sheetState = sheetState
+                    ) {
+                        bottomSheetContent {
+                            showBottomSheet = false
+                        }
+                    }
+                }
             }
+        }
+    }
+}
+
+@Composable
+fun bottomSheetContent(onCloses: () -> Unit) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(250.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(text = "Bottom Sheet")
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Button(
+            onClick = onCloses
+        ) {
+            Text(text = "Close Bottom Sheet")
         }
     }
 }
